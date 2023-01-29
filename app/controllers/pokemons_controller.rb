0a71d9b0c2ef4b29196813935dce89b3d2322dbf
camilogzlez <camilogzlez@gmail.com>
2023-01-29 13:48:42 -0300

    show action implemented without refactoring

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -8,16 +8,29 @@ class PokemonsController < ApplicationController
     @pokemon = JSON.parse(response.body, symbolize_names: true)
     @pokemons = @pokemon.values[3]
     @pokemons_total = []
-    
 
     @pokemons.each do |pokemon|
       response_2 = Faraday.get(pokemon.values[1])
      @pokemon_2 = JSON.parse(response_2.body, symbolize_names: true)
       @pokemons_total << @pokemon_2
     end 
-
     #byebug
-  
+  end
+
+  def show
+    pokemon = params[:id]
+    #  byebug
+    conn = Faraday.new("https://pokeapi.co")
+    response = conn.get("/api/v2/pokemon/#{pokemon}")
+    @pokemon = JSON.parse(response.body, symbolize_names: true)
+
+    response_2 = conn.get("/api/v2/evolution-chain/#{pokemon}")
+    @pokemon_evolutions = JSON.parse(response_2.body, symbolize_names: true)
+
+    response_3 = conn.get("/api/v2/pokemon-species/#{pokemon}")
+    @pokemon_especies = JSON.parse(response_3.body, symbolize_names: true)
+
+
 
   end
 
